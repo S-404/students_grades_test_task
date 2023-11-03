1a52ffecbe73c4373c81700fd008c0660b21f9f5
andrey_s <sudarev.andrey.i@gmail.com>
2023-11-03 21:25:11 +0300

    add nats init to index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 require('dotenv').config()
 
+const natsClient = require('./nats/index')
 const apiErrorMiddleware = require('./middlewares/error-middleware')
 const router = require('./routes/index')
 const PORT = process.env.PORT
@@ -9,6 +10,9 @@ const app = express()
 app.use(express.json())
 app.use(router)
 app.use(apiErrorMiddleware)
+
+
+natsClient.init()
 
 app.listen(PORT, function () {
     console.log(`Server is running on port ${PORT}`)
